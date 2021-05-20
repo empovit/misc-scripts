0784bd3c74d4a77d6f5cbfebb8ed2fc1a4859240
Vitaliy Emporopulo <vemporop@redhat.com>
2021-05-20 18:26:04 +0300

    Simplify, ingition version 3.2.0, remove ttyS0

--- enable-ignition-timeout.sh
+++ enable-ignition-timeout.sh
@@ -31,7 +31,7 @@ fi
 CLUSTER_URL=$BASE_URL/api/assisted-install/v1/clusters/$CLUSTER
 
 echo "Getting hosts from $CLUSTER_URL"
-HOSTS=`curl -sSf -X GET -H "Content-Type: application/json" -H "authorization: ${BEARER}" "${CLUSTER_URL}" | jq "." | grep '"id"' | awk -F'"' '{print $4}'`
+HOSTS=`curl -sSf -X GET -H "Content-Type: application/json" -H "authorization: ${BEARER}" "${CLUSTER_URL}" | jq "." | grep '"id"' | grep -v "$CLUSTER" | awk -F'"' '{print $4}'`
 
 if [[ -z ${HOSTS} ]]; then
     echo "ERROR: No hosts could be read" >&2
@@ -41,21 +41,18 @@ fi
 echo "Updating the hosts"
 for h in ${HOSTS}
 do
-    if [ "$h" != "$CLUSTER" ]; then
+    echo
+    echo "Host: $h"
+    curl -X PATCH \
+        -H "Content-Type: application/json" \
+        --data-binary '{"config": "{\"ignition\":{\"timeouts\":{\"httpTotal\":120},\"version\":\"3.2.0\"}}"}' \
+        -H "authorization: Bearer ${BEARER}" \
+        "$BASE_URL/api/assisted-install/v1/clusters/$CLUSTER/hosts/$h/ignition"
 
-        echo
-        echo "Host: $h"
-        curl -X PATCH \
-            -H "Content-Type: application/json" \
-            --data-binary '{"config": "{\"ignition\":{\"timeouts\":{\"httpTotal\":300},\"version\":\"3.1.0\"}}"}' \
-            -H "authorization: Bearer ${BEARER}" \
-            "$BASE_URL/api/assisted-install/v1/clusters/$CLUSTER/hosts/$h/ignition"
-
-        echo
-        curl -X PATCH \
-            -H "Content-Type: application/json" \
-            --data-binary '{"args": ["--delete-karg", "console=ttyS0,115200n8", "--append-karg", "rd.shell", "--append-karg", "rd.debug", "--append-karg", "rd.net.timeout.carrier=30"]}' \
-            -H "authorization: Bearer ${BEARER}" \
-            "$BASE_URL/api/assisted-install/v1/clusters/$CLUSTER/hosts/$h/installer-args"
-    fi
+    echo
+    curl -X PATCH \
+        -H "Content-Type: application/json" \
+        --data-binary '{"args": ["--append-karg", "rd.shell", "--append-karg", "rd.debug"]}' \
+        -H "authorization: Bearer ${BEARER}" \
+        "$BASE_URL/api/assisted-install/v1/clusters/$CLUSTER/hosts/$h/installer-args"
 done
